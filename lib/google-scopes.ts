4aed00101dc26dbdd6b61818e5be1894e37b939e
Adeildo <adeildo@ranqia.com>
2026-04-05 20:27:07 -0300

    feat: add google scopes constants and mem0 client

---
+++ lib/google-scopes.ts
@@ -0,0 +1,4 @@
+export const GOOGLE_SCOPES = {
+  DRIVE_READONLY: "https://www.googleapis.com/auth/drive.readonly",
+  DOCS_READONLY: "https://www.googleapis.com/auth/documents.readonly",
+} as const
---
+++ lib/mem0.ts
@@ -0,0 +1,3 @@
+import MemoryClient from "mem0ai"
+
+export const mem0 = new MemoryClient({ apiKey: process.env.MEM0_API_KEY! })
